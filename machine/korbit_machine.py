cc478f8e34b6a6ba935adf106d072b39709917cf
ricepotato <sukjun40@naver.com>
2019-10-20 03:11:14 +0900

    거래 메소드 구현

--- machine/korbit_machine.py
+++ machine/korbit_machine.py
@@ -1,10 +1,10 @@
-
+import time
 import requests
 import configparser
 
 from machine.base_machine import Machine
 
-class KorbitMachine(Machine):
+class KorbitMachine:
     """ 코빗 거래소와의 거래를 위한 클래스입니다.
     BASE_API_URL 은 REST API 호출을 위한 기본 URL 입니다.
     TRADE_CURRENCY_TYPE 은 코빗에서 거래가 가능한 화폐의 종류입니다.
@@ -65,5 +65,110 @@ class KorbitMachine(Machine):
 
     def get_ticker(self, currency_type=None):
         """ 마지막 결제정보(Tick)를 구하는 메서드입니다. """
+        if currency_type is None:
+            raise Exception("Need to currency_type")
 
-        
+        time.sleep(1)
+
+        params = {"currency_pair":currency_type}
+        ticker_api_path = "/v1/ticker/detailed"
+        url_path = self.BASE_API_URL + ticker_api_path
+        res = requests.get(url_path, params=params)
+        response_json = res.json()
+        result = {}
+        result["timstamp"] = str(response_json["timestamp"])
+        result["last"] = response_json["last"]
+        result["bid"] = response_json["bid"]
+        result["ask"] = response_json["ask"]
+        result["high"] = response_json["high"]
+        result["low"] = response_json["low"]
+        result["volume"] = response_json["volume"]
+        return result
+
+    def get_filled_orders(self, currency_type=None, per="minute"):
+        """ 채결 정보를 구하는 메서드 입니다. """
+
+        if currency_type is None:
+            raise Exception("Need to currency_type")
+        time.sleep(1)
+        params = {"currency_pair":currency_type, "time":per}
+        orders_api_path = "/v1/transactions"
+        url_path = self.BASE_API_URL + orders_api_path
+        res = requests.get(url_path, params=params)
+        result = res.json()
+        return result
+
+    def get_wallet_status(self):
+        """ 사용자의 지갑정보를 조회하는 메서드 입니다. """
+
+        time.sleep(1)
+        wallet_status_api_path = "/v1/user/balances"
+        url_path = self.BASE_API_URL + wallet_status_api_path
+        if self.access_token is None:
+            self.set_token()
+        headers = {"Authorization":"Bearer " + self.access_token}
+        res = requests.get(url_path, headers=headers)
+        result = res.json()
+        wallet_status = {}
+        for currency in self.TRADE_CURRENCY_TYPE:
+            wallet_status[currency] = {}
+            wallet_status[currency]["available"] = result[currency]["available"]
+            wallet_status[currency]["trade_in_use"] = float(result[currency]["trade_in_use"])
+            wallet_status[currency]["withdrawal_in_use"] = float(result[currency]["withdrawal_in_use"])
+            wallet_status[currency]["balance"] = float(result[currency]["trade_in_use"]) + float(result[currency]["withdrawal_in_use"])
+            
+        return wallet_status
+
+    def buy_order(self, currency_type: str, price: str, qty: str,
+                  order_type="limit"):
+        """ 매수 주문 실행.
+        지정가 거래 지원 
+        price : 1개 수량 주문 원화 (krw) 값
+        qty : 주문 수량
+        """
+
+        time.sleep(1)
+        if currency_type is None or price is None or qty is None:
+            raise Exception("Need to param")
+        buy_order_api_path = "/v1/user/orders/buy"
+        url_path = self.BASE_API_URL + buy_order_api_path
+        if self.access_token is None:
+            self.set_token()
+        headers = {"Authorization":"Bearer " + self.access_token}
+        data = {
+            "currency_pair":currency_type, "type":order_type,
+            "price":price, "coin_amount":qty,
+            "nonce":self.get_nonce()
+        }
+        res = requests.post(url_path, headers=headers, data=data)
+        result = res.json()
+        return result
+
+    def sell_order(self, currency_type: str, price: str, qty: str,
+                   order_type="limit"):
+        """ 매도 주문 실행 메소드 
+
+        price : 1개 주문 수량 원화
+        qty : 주문 수량
+        """
+        time.sleep(1)
+        if price is None or qty is None or currency_type is None:
+            raise Exception("Need to params")
+        if order_type != "limit":
+            raise Exception("check order type")
+        sell_order_api_path = "/v1/user/orders/sell"
+        url_path = self.BASE_API_URL + sell_order_api_path
+        if self.access_token is None:
+            self.set_token()
+        headers = {"Authorization":"Bearer " + self.access_token}
+        data = {
+            "currency_pair":currency_type, "type":order_type,
+            "price":price, "coin_amount":qty,
+            "nonce":self.get_nonce()
+        }
+        res = requests.post(url_path, headers=headers, data=data)
+        result = res.json()
+        return result
+
+    def get_nonce(self):
+        return str(int(time.time()))
--- tests/test_korbit_machine.py
+++ tests/test_korbit_machine.py
@@ -14,3 +14,43 @@ class KorbitMachineTestCase(unittest.TestCase):
         expire, access_token, refresh_token = self.korbit_machine.set_token(grant_type="client_credentials")
         assert access_token
         print("Expire:", expire, "Access_token:", access_token, "Refresh_token:", refresh_token)
+
+    def test_get_ticker(self):
+        print(inspect.stack()[0][3])
+        ticker = self.korbit_machine.get_ticker("etc_krw")
+        assert ticker
+        print(ticker)
+
+    def test_get_filled_orders(self):
+        print(inspect.stack()[0][3])
+        order_book = self.korbit_machine.get_filled_orders(currency_type="btc_krw")
+        assert order_book
+        print(order_book)
+
+    def test_get_wallet_status(self):
+        print(inspect.stack()[0][3])
+        wallet_status = self.korbit_machine.get_wallet_status()
+        assert wallet_status
+        print(wallet_status)
+
+    def test_buy_order(self):
+        print(inspect.stack()[0][3])
+        buy_order = self.korbit_machine.buy_order(currency_type="etc_krw", 
+                                                  price="15000",
+                                                  qty="1", order_type="limit")
+        assert buy_order
+        print(buy_order)
+
+    def test_sell_order(self):
+        print(inspect.stack()[0][3])
+        sell_order = self.korbit_machine.sell_order(currency_type="etc_krw",
+                                                    price="40000",
+                                                    qty="1")
+        assert sell_order
+        print(sell_order)
+        
+    def test_get_nonce(self):
+        print(inspect.stack()[0][3])
+        nonce = self.korbit_machine.get_nonce()
+        assert nonce
+        print(nonce)
